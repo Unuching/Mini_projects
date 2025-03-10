61cde66ec6b3f5ee5154d08159648fc544def49a
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-10 09:03:36 +0600

    Update script.js in blurry_loading directory

--- blurry_loading/script.js
+++ blurry_loading/script.js
@@ -1,3 +1,11 @@
 const load_text = document.querySelector(".loading_text");
 const background = document.querySelector(".bg");
 
+let load = 0;
+
+let int = setInterval(blurring, 30);
+function blurring() {
+  load++;
+  console.log(load);
+  
+}
