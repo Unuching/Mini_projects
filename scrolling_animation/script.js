b3245c02f88445f54ea89d9acfdbd095152e8e57
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-11 10:51:09 +0600

    Update modified scrolling_animation files

--- scrolling_animation/script.js
+++ scrolling_animation/script.js
@@ -1,7 +1,8 @@
-const my_boxes = document.querySelectorAll("box")
+const my_boxes = document.querySelectorAll("box");
 
-window.addEventListener("scroll", checkboxes)
+window.addEventListener("scroll", checkboxes);
 
-function checkboxes(){
-    
+checkboxes();
+function checkboxes() {
+  const trigger_bottom = (window.innerHeight / 5) * 4;
 }
