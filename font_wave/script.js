3a40f2730715996985b6b6e6d21e55586b762dc5
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-12 10:54:25 +0600

    Update modified script.js and style.css in font_wave directory

--- font_wave/script.js
+++ font_wave/script.js
@@ -1,5 +1,11 @@
 const labels = document.querySelectorAll(".form_control label");
 
-labels.forEach((label)=>{
-    label.innerHTML = label.innerText
-})
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 60}ms">${letter}</span>`
+    )
+    .join("");
+});
