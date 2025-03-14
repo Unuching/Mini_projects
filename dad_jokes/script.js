fc144839c96d32cff8befda5ba7751ca8ad37bb3
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-14 08:20:47 +0600

    Update modified dad jokes files

--- dad_jokes/script.js
+++ dad_jokes/script.js
@@ -1,6 +1,21 @@
 const myJokes = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
-generateNewJoke()
-function generateNewJoke(){
-    
+
+jokeBtn.addEventListener("click", generateNewJoke);
+generateNewJoke();
+async function generateNewJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await res.json();
+  myJokes.innerHTML = data.joke;
 }
+//     .then((res) => res.json())
+//     .then((data) => {
+//       myJokes.innerHTML = data.joke;
+//     });
+// }
