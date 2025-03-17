278fa11c290a0ef19bccc59889320eb3d5723198
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-17 08:29:38 +0600

    update code to codebase

--- randon_choice_picker/script.js
+++ randon_choice_picker/script.js
@@ -5,6 +5,13 @@ textArea.focus();
 
 textArea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
 });
 function createTags(input) {
   const tags = input
@@ -20,4 +27,33 @@ function createTags(input) {
     tag_EL.innerText = tag;
     tagsEL.appendChild(tag_EL);
   });
+}
+function randomSelect() {
+  const highlightTimes = 40;
+  const interval = setInterval(() => {
+    const randomTag = pickRandonTag();
+    highlightOn(randomTag);
+    setTimeout(() => {
+      highlightOff(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandonTag();
+      highlightOn(randomTag);
+    }, 100);
+  }, highlightTimes * 100);
+}
+function pickRandonTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightOn(tag) {
+  tag.classList.add("highlight");
+}
+function highlightOff(tag) {
+  tag.classList.remove("highlight");
 }
