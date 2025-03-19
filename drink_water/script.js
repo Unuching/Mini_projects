c234fb7e1b62ebd371c67f2d23e0a7905269d366
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-19 12:03:42 +0600

    update script.js

--- drink_water/script.js
+++ drink_water/script.js
@@ -3,6 +3,7 @@ const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
+updateBigCup();
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => {
     Highlight_cups(idx);
@@ -23,4 +24,27 @@ function Highlight_cups(idx) {
       cup.classList.remove("full");
     }
   });
+  updateBigCup();
 }
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup_small.full").length;
+  const totalCups = smallCups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 230}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups == totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+  }
+}
+
+
