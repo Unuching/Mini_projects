b781cf649e03e50017047197165a7d2b1f64c46c
Unuching Marma <unuching.marma2023@gmail.com>
2025-03-19 11:45:26 +0600

    update drink_water script

--- drink_water/script.js
+++ drink_water/script.js
@@ -9,6 +9,13 @@ smallCups.forEach((cup, idx) => {
   });
 });
 function Highlight_cups(idx) {
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
   smallCups.forEach((cup, idx2) => {
     if (idx2 <= idx) {
       cup.classList.add("full");
